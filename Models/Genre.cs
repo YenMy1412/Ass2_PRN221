1942d39049696515d86403a4a737ecc0289c881a
Nguyen Van Duan <119272062+NguyenVanDuan11@users.noreply.github.com>
2023-02-14 23:56:03 +0700

    .update

---
+++ Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment2_Group4_SE1610.Models
+{
+    public partial class Genre
+    {
+        public Genre()
+        {
+            Albums = new HashSet<Album>();
+        }
+
+        public int GenreId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
