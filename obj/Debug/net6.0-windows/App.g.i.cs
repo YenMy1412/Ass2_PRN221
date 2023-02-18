05db17f32841971e591e9e1345faf111eb055905
Nguyen Van Duan <119272062+NguyenVanDuan11@users.noreply.github.com>
2023-02-18 22:48:38 +0700

    .

--- obj/Debug/net6.0-windows/App.g.i.cs
+++ obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Assignment2_Group4_SE1610 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Assignment2_Group4_SE1610 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public static void Main() {
             Assignment2_Group4_SE1610.App app = new Assignment2_Group4_SE1610.App();
             app.InitializeComponent();
